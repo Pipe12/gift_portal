61bb5e3559115dbc30df947419bc6235521e4278
Andres Burbano <ciento1@hotmail.com>
2018-10-26 20:41:38 -0500

    Add features to category model. create, update and delete.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,45 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to root_path
+    else
+      redirect_to new category_path
+    end
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      redirect_to edit_category_path
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'hello#prueba'
+
+  resources :categories, except: [:show]
 end
