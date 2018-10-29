7186f61b6f1146aa58d1cba7211ebcf35d0f6df1
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2018-10-28 20:12:12 -0500

    finish a products CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
 
     if @product.save
       puts 'Vamos bien'
-      redirect_to categories_path
+      redirect_to products_path
     else
       puts 'Something is wrong'
     end
@@ -16,6 +16,31 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(products_params)
+      redirect_to product_path
+    else
+      redirect_to edit_product_path
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+    redirect_to products_path
   end
 
   private
